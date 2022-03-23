2b3a90ea3f9f115b89b2d8c9d481ba3bd6bc752f
mahdi-suleiman <mahdi.suleimann@gmail.com>
2022-03-23 09:28:30 +0300

    minor changes

--- frontend/pages/feed.jsx
+++ frontend/pages/feed.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { ApolloClient, InMemoryCache, createHttpLink, gql } from "@apollo/client";
-import client from '../apollo-client';
+// import client from '../apollo-client';
 import { setContext } from '@apollo/client/link/context';
 import Image from 'next/image'
 import { useRouter } from 'next/router';
